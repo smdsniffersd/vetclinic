14defc28497c1207f5f974afc3790d5be13d6b61
smdsniffersd <164409560+smdsniffersd@users.noreply.github.com>
2026-05-30 05:54:28 +0300

    Add files via upload

---
+++ views/errors/404Error.php
@@ -0,0 +1,27 @@
+<html>
+<header>
+
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/404Error.css">
+
+</header>
+<body>
+    <div class="text-div">
+        <span class="number">404</span>
+        <span>Sorry, the page is unavailable or does not exist.</span>
+    </div>
+    <div class="midle-lapka idle-lapka-first"></div>
+    <div class="midle-lapka idle-lapka-second"></div>
+    <div class="small-lapka small-lapka-fisrst"></div>
+    <div class="small-lapka small-lapka-second"></div>
+    <div class="small-lapka small-lapka-tree"></div>
+    <div class="small-lapka small-lapka-fourth"></div>
+    <div class="bg-lapka-1"></div>
+    <div class="bg-lapka-2"></div>
+    <div class="bg-lapka-3"></div>
+    <div class="bg-lapka-4"></div>
+</body>
+
+</html>
+
+
